b7f91899391bc863863e498c3dc6cf396e3947ef
shakram02 <ahmedhamdyau@gmail.com>
2018-03-25 09:26:56 +0200

    implemented A*

--- src/main/kotlin/npuzzle/solvers.kt
+++ src/main/kotlin/npuzzle/solvers.kt
@@ -3,6 +3,8 @@ package npuzzle
 import npuzzle.abstractions.State
 import npuzzle.datastructures.Queue
 import npuzzle.datastructures.Stack
+import java.util.*
+import kotlin.collections.HashSet
 
 fun <T : Number> bfs(initialState: State<T>, isAtGoal: (State<T>) -> Boolean): Collection<State<T>> {
     val frontier = Queue(mutableListOf(initialState))
@@ -32,6 +34,9 @@ fun <T : Number> bfs(initialState: State<T>, isAtGoal: (State<T>) -> Boolean): C
 fun <T : Number> dfs(initialState: State<T>, isAtGoal: (State<T>) -> Boolean): Collection<State<T>> {
     val frontier = Stack(mutableListOf(initialState))
     val explored = HashSet<State<T>>()
+    val possibleTargets = Queue(initialState.move.getResult().toMutableList())
+    val visitedTargets = HashSet<T>()
+    visitedTargets.add(possibleTargets.dequeue()!!)
 
     while (frontier.isNotEmpty()) {
         val state = frontier.pop()!!
@@ -41,11 +46,38 @@ fun <T : Number> dfs(initialState: State<T>, isAtGoal: (State<T>) -> Boolean): C
             return getSolutionPath(state)
         }
 
-        for (neighbor in state.getFrontier().reversed()) {
+        for (neighbor in state.getFrontier()) {
             if (explored.contains(neighbor) || frontier.contains(neighbor)) {
                 continue
             }
             frontier.push(neighbor)
+        }
+
+    }
+
+    return mutableListOf()
+}
+
+fun <T : Number> aStar(initialState: State<T>, isAtGoal: (State<T>) -> Boolean,
+                       costFunction: (Pair<Int, Int>, Pair<Int, Int>) -> Int): Collection<State<T>> {
+    val frontier: PriorityQueue<State<T>> = PriorityQueue(StateComparator(costFunction))
+    frontier.add(initialState)
+    val explored = HashSet<State<T>>()
+
+    while (frontier.isNotEmpty()) {
+        val state = frontier.remove()
+        explored.add(state)
+
+        if (isAtGoal(state)) {
+            return getSolutionPath(state)
+        }
+
+        for (neighbor in state.getFrontier()) {
+            if (explored.contains(neighbor) || frontier.contains(neighbor)) {
+                continue
+            }
+
+            frontier.add(neighbor)
         }
     }
 
