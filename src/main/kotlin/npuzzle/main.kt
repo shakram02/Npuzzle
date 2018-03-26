f6f0d5707e7046a539d1a0d69682e5c8344d3c4b
shakram02 <ahmedhamdyau@gmail.com>
2018-03-26 09:44:44 +0200

    print example names

--- src/main/kotlin/npuzzle/main.kt
+++ src/main/kotlin/npuzzle/main.kt
@@ -8,29 +8,34 @@ import npuzzle.abstractions.State
 import java.util.*
 
 fun main(args: Array<String>) {
-    val graph = arrayOf(1, 2, 5, 3, 4, 0, 6, 7, 8)
-//    val graph =arrayOf(1, 4, 2, 6, 5, 8, 7, 3, 0)
-//    graph.swap(2, 3)
-//    println(graph.contentToString())
-
-//    println(Move(graph, 0, Direction.North))
-//    val state = State(graph, Move(graph, 0, Direction.None))
-//    println(state.move(0, Direction.North).move)
+    val graph = arrayOf(1, 2, 5, 3, 4, 0, 6, 7, 8)    // Easy case
+//    val graph = arrayOf(1, 4, 2, 6, 5, 8, 7, 3, 0)  // Hard case
 
     val initialState = State(0, graph, Move(graph, 0, Direction.None))
-
-//    val sameState = State(graph, Move(graph, 0, Direction.None))
-//    val set = HashSet(mutableListOf(initialState))
-//    println(set.contains(sameState))
-
-//    println(frontier)
     val finalState = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
+
+    printBanner("BFS")
     val path = bfs(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) })
     println(path)
 
-    // TODO, check this condition
+    printBanner("A* - Manhattan")
+    val pathAStar = aStar(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) },
+            { x, y -> manhattanDistance(x, y) })
+    println(pathAStar)
+
+    printBanner("A* - Euclidean")
+    val pathAStarEuclidean = aStar(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) },
+            { x, y -> euclideanDistance(x, y) })
+    println(pathAStarEuclidean)
+
+
+    printBanner("DFS")
+    // FIXME: DFS gets stuck!
     val pathDfs = dfs(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) })
     println(pathDfs)
-
 }
 
+fun printBanner(algorithmName: String) {
+    println("\n$algorithmName\n")
+    println("-".repeat(12))
+}
