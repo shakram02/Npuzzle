e1fded9744f4f5bce3b7b07e6cdfe8b57585eda3
shakram02 <ahmedhamdyau@gmail.com>
2018-04-03 10:43:52 +0200

    update main example

--- src/main/kotlin/npuzzle/main.kt
+++ src/main/kotlin/npuzzle/main.kt
@@ -2,37 +2,37 @@
 
 package npuzzle
 
-import npuzzle.abstractions.Direction
-import npuzzle.abstractions.Move
-import npuzzle.abstractions.State
+import npuzzle.abstractions.*
 import java.util.*
 
 fun main(args: Array<String>) {
-    val graph = arrayOf(1, 2, 5, 3, 4, 0, 6, 7, 8)    // Easy case
+    val graph = arrayOf(1, 2, 5, 3, 0, 4, 6, 7, 8)    // Easy case
 //    val graph = arrayOf(1, 4, 2, 6, 5, 8, 7, 3, 0)  // Hard case
+    val finalGraph = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
 
-    val initialState = State(0, graph, Move(graph, 0, Direction.None))
-    val finalState = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
+    val manhattanCalculator = PuzzleCostCalculator({ x, y -> manhattanDistance(x, y) })
+    val euclideanCalculator = PuzzleCostCalculator({ x, y -> euclideanDistance(x, y) })
+
+    val initialState = State(PuzzleArray(0, graph))
+    val finalState = State(PuzzleArray(0, finalGraph))
 
     printBanner("BFS")
-    val path = bfs(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) })
+    val path = bfs(initialState, { x -> finalState == x })
     println(path)
 
     printBanner("A* - Manhattan")
-    val pathAStar = aStar(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) },
-            { x, y -> manhattanDistance(x, y) })
+    val pathAStar = aStar(initialState, { x -> finalState == x }, manhattanCalculator)
     println(pathAStar)
 
     printBanner("A* - Euclidean")
-    val pathAStarEuclidean = aStar(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) },
-            { x, y -> euclideanDistance(x, y) })
+    val pathAStarEuclidean = aStar(initialState, { x -> finalState == x }, euclideanCalculator)
     println(pathAStarEuclidean)
 
 
     printBanner("DFS")
-    // FIXME: DFS gets stuck!
-    val pathDfs = dfs(initialState, { x -> Arrays.equals(finalState, x.move.getResult()) })
-    println(pathDfs)
+    // DFS gets stuck! because of the huge number of cases
+//    val pathDfs = dfs(initialState, { x -> finalState == x })
+//    println(pathDfs)
 }
 
 fun printBanner(algorithmName: String) {
