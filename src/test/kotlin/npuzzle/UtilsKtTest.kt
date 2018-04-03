38f9a5b97bc1540bd97831fa03f131f77275c55d
shakram02 <ahmedhamdyau@gmail.com>
2018-04-03 10:43:42 +0200

    update tests

--- src/test/kotlin/npuzzle/UtilsKtTest.kt
+++ src/test/kotlin/npuzzle/UtilsKtTest.kt
@@ -1,5 +1,7 @@
 package npuzzle
 
+import npuzzle.abstractions.PuzzleArray
+import npuzzle.abstractions.PuzzleCostCalculator
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -7,21 +9,22 @@ class UtilsKtTest {
     @Test
     fun indexToPair() {
         val array = arrayOf(1, 2, 3, 4)
-
-        assertEquals(Pair(0, 0), array.indexToPair(0))
-        assertEquals(Pair(0, 1), array.indexToPair(1))
-        assertEquals(Pair(1, 0), array.indexToPair(2))
-        assertEquals(Pair(1, 1), array.indexToPair(3))
+        val puzzle = PuzzleArray(0, array)
+        assertEquals(Pair(0, 0), puzzle.indexToPair(0))
+        assertEquals(Pair(0, 1), puzzle.indexToPair(1))
+        assertEquals(Pair(1, 0), puzzle.indexToPair(2))
+        assertEquals(Pair(1, 1), puzzle.indexToPair(3))
     }
 
     @Test
     fun manhattanDistanceTest() {
-        val array = Array(10, { i -> i })   // No shifts
-        assertEquals(0, array.calculateCost({ x, y -> manhattanDistance(x, y) }))
+        val array = PuzzleArray(0, Array(10, { i -> i }))   // No shifts
+        val manhattanCalculator = PuzzleCostCalculator({ x, y -> manhattanDistance(x, y) })
+        assertEquals(0, manhattanCalculator.calculateCost(array))
 
-        val misplacedRow = arrayOf(1, 2, 3)
+        val misplacedRow = PuzzleArray(0, arrayOf(1, 2, 3))
         // Every item is shifted 1 cell in the X direction
-        assertEquals(3, misplacedRow.calculateCost({ x, y -> manhattanDistance(x, y) }))
+        assertEquals(3, manhattanCalculator.calculateCost(misplacedRow))
 
         /*
             Truth:      Actual:
@@ -35,17 +38,18 @@ class UtilsKtTest {
                4: (1, 1) - (2, 0)   => 2
                Error: 6
          */
-        val misplacedCols = arrayOf(1, 2, 3, 4)
-        assertEquals(6, misplacedCols.calculateCost({ x, y -> manhattanDistance(x, y) }))
+        val misplacedCols = PuzzleArray(0, arrayOf(1, 2, 3, 4))
+        assertEquals(6, manhattanCalculator.calculateCost(misplacedCols))
     }
 
     @Test
     fun euclideanDistanceTest() {
-        val array = Array(10, { i -> i })
-        assertEquals(0, array.calculateCost({ x, y -> euclideanDistance(x, y) }))
+        val array = PuzzleArray(0, Array(10, { i -> i }))
+        val euclideanCalculator = PuzzleCostCalculator({ x, y -> euclideanDistance(x, y) })
+        assertEquals(0, euclideanCalculator.calculateCost(array))
 
-        val misplacedRow = arrayOf(1, 2, 3) // Shifts in 1D, equivalent to manhattan
-        assertEquals(3, misplacedRow.calculateCost({ x, y -> euclideanDistance(x, y) }))
+        val misplacedRow = PuzzleArray(0, arrayOf(1, 2, 3)) // Shifts in 1D, equivalent to manhattan
+        assertEquals(3, euclideanCalculator.calculateCost(misplacedRow))
 
         /*
            Truth:      Actual:
@@ -54,7 +58,7 @@ class UtilsKtTest {
 
                Diagonals represent an error by 1 instead of 2 in manhattan distance
         */
-        val misplacedCols = arrayOf(1, 2, 3, 4)
-        assertEquals(4, misplacedCols.calculateCost({ x, y -> euclideanDistance(x, y) }))
+        val misplacedCols = PuzzleArray(0, arrayOf(1, 2, 3, 4))
+        assertEquals(4, euclideanCalculator.calculateCost(misplacedCols))
     }
 }
